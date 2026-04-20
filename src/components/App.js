faa357c9027f9467ec0be4826862ecbefe4a47be
Archit <archit483@gmail.com>
2026-04-20 16:24:15 +0530

    button-action-functional

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,17 @@ import "./../styles/App.css";
 
 function App() {
 
-  let [click, setClick] = useState()
+  let [click, setClick] = useState(false)
 
   function handleClick(){
-    click = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
-    setClick(click)
+    // click = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    setClick(!click)
   }
 
   return(
     <div id="main">
-      <p id="para">{click}</p>
+      {click && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+      {/* <p id="para">{click}</p> */}
       <button id="click" onClick={handleClick}>Click</button>
     </div>
   );
